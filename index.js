716af0e4fb8dfcf8c8192c6c8dfbfc5b9f8f5eb5
Adasat <pbonbol@gmail.com>
2024-05-17 16:09:43 +0100

    Fixed the bug!!

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function createTable() {
 
   for (let i = 0; i < 40; i++) {
     tableHTML += `<div class='cardContainer'>
-        <div  id='${data[i].nombre.split(' ')[0]}' class='card '>
-         <img src=${data[i].img} class='image' hidden />
+        <div  id='${data[i].nombre.split(' ')[0]}_${i}' class='card pulse'>
+         <img src=${data[i].img} class='image' hidden/>
         </div>
       </div>`
   }
@@ -30,27 +30,37 @@ function showCard(item) {
   let lastImage = item.querySelector('img')
 
   if (double.length < 2){
+    console.log('entro aquí')
     lastImage.removeAttribute('hidden')
     item.getAttribute('id')
     double.push(item.getAttribute('id'))
   }
-  console.log(double[0] === double[1])
+  console.log(double[0].split('_')[0])
   console.log(double)
 
-  if (double.length === 2 && double[0] === double[1]){
+  if (double.length === 2 && double[0].split('_')[0] === double[1].split('_')[0]){
 
     counter += 1
     double = []
   } 
 
-  if (double.length === 2 && double[0] !== double[1]) {
+  if (
+    double.length === 2 &&
+    double[0].split('_')[0] !== double[1].split('_')[0]
+  ) {
     let oldImage = document.querySelector(`#${double[0]} img`)
-    console.log(oldImage)
+    let oldItem = document.querySelector(`#${double[0]}`)
+    console.log('Vieja carta ', oldImage)
+    console.log('Última carta ', lastImage)
+
     setTimeout(() => {
       oldImage.setAttribute('hidden', true)
       lastImage.setAttribute('hidden', true)
-      double = [] // Limpia el array después de ocultar las imágenes
+      item.classList.remove('pulse')
+      // Limpia el array después de ocultar las imágenes
     }, 1000)
+
+    double = []
   }
 
   
